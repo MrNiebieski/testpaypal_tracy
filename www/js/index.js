697364ab345d3ae65d739809733f904aaeaeb636
tracyhuang9 <tracyhuang9@gmail.com>
2015-01-23 00:29:04 -0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -58,7 +58,7 @@ var app = {
     alert("payment success: " + JSON.stringify(payment, null, 4));
     console.log("payment success: " + JSON.stringify(payment, null, 4));
     var newdata = JSON.parse(payment).id;
-    alert{id);
+    alert{String(id));
   },
   
   onAuthorizationCallback: function(authorization) {
